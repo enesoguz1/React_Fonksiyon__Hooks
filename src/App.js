75eb7ab8cb16d6a2f4dadbc7ed8427c84cc7fdf1
Enes Oğuz <enesoguz10@gmail.com>
2022-11-28 12:13:04 +0300

    Fonksiyon Oluşturma

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <h2>Node.js</h2>
+      <p>React.js</p>
+
+    </div>
+  );
+}
+export default App;
---
+++ src/Buton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Buton() {
+
+    const buton=()=>{
+        console.log('Reack Hooks öğreniyorum...');
+
+}
+  return (
+    <div>
+      <button onClick={buton}>Buton</button>
+    </div>
+  )
+}
+
+export default Buton;
